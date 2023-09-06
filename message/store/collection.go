9ecbb356d46787c00d91336ee3acdfed5a1691b3
Dusan Brajovic <dbrajovic3@gmail.com>
2023-09-06 23:45:53 +0200

    refactor

--- message/store/collection.go
+++ message/store/collection.go
@@ -7,33 +7,29 @@ type msg interface {
 }
 
 func newCollection[M msg]() collection[M] {
-	return collection[M]{
-		collection: map[uint64]map[uint64]*msgSet[M]{},
-	}
+	return map[uint64]map[uint64]msgSet[M]{}
 }
 
-type collection[M msg] struct {
-	collection map[uint64]map[uint64]*msgSet[M]
-}
+type collection[M msg] map[uint64]map[uint64]msgSet[M]
 
 func (c *collection[M]) addMessage(msg *M, view *types.View, from []byte) {
-	sameSequenceMessages, ok := c.collection[view.Sequence]
+	sameSequenceMessages, ok := (*c)[view.Sequence]
 	if !ok {
-		c.collection[view.Sequence] = map[uint64]*msgSet[M]{}
-		sameSequenceMessages = c.collection[view.Sequence]
+		(*c)[view.Sequence] = map[uint64]msgSet[M]{}
+		sameSequenceMessages = (*c)[view.Sequence]
 	}
 
 	sameRoundMessages, ok := sameSequenceMessages[view.Round]
 	if !ok {
-		sameSequenceMessages[view.Round] = &msgSet[M]{s: map[string]*M{}}
-		sameRoundMessages = sameSequenceMessages[view.Round]
+		(*c)[view.Sequence][view.Round] = msgSet[M]{}
+		sameRoundMessages = (*c)[view.Sequence][view.Round]
 	}
 
-	sameRoundMessages.s[string(from)] = msg
+	sameRoundMessages[string(from)] = msg
 }
 
 func (c *collection[M]) getMessages(view *types.View) []*M {
-	sameSequenceMessages, ok := c.collection[view.Sequence]
+	sameSequenceMessages, ok := (*c)[view.Sequence]
 	if !ok {
 		return nil
 	}
@@ -43,14 +39,16 @@ func (c *collection[M]) getMessages(view *types.View) []*M {
 		return nil
 	}
 
-	messages := make([]*M, 0, len(sameRoundMessages.s))
-	for _, msg := range sameRoundMessages.s {
+	return sameRoundMessages.messages()
+}
+
+type msgSet[M msg] map[string]*M
+
+func (s msgSet[M]) messages() []*M {
+	messages := make([]*M, 0, len(s))
+	for _, msg := range s {
 		messages = append(messages, msg)
 	}
 
 	return messages
-}
-
-type msgSet[M msg] struct {
-	s map[string]*M
 }
