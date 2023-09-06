423c2c4690050f1f1f9d17cef3b67f8a676c0218
Dusan Brajovic <dbrajovic3@gmail.com>
2023-09-06 16:50:58 +0200

    util tests

--- message/types/util.go
+++ message/types/util.go
@@ -1,5 +1,17 @@
 package types
 
+import "google.golang.org/protobuf/proto"
+
+type FinalizedSeal struct {
+	From, CommitSeal []byte
+}
+
+type FinalizedBlock struct {
+	Block []byte
+	Seals []FinalizedSeal
+	Round uint64
+}
+
 type Msg interface {
 	Bytes() []byte
 	GetFrom() []byte
@@ -28,4 +40,164 @@ func ToMsg[M ibft](messages []*M) []Msg {
 	}
 
 	return wrapped
+}
+
+func (x *MsgProposal) Bytes() []byte {
+	bz, _ := proto.Marshal(&MsgProposal{
+		View:                   x.View,
+		From:                   x.From,
+		Signature:              x.Signature,
+		ProposedBlock:          x.ProposedBlock,
+		BlockHash:              x.BlockHash,
+		RoundChangeCertificate: x.RoundChangeCertificate,
+	})
+
+	return bz
+}
+
+func (x *MsgProposal) Payload() []byte {
+	bz, _ := proto.Marshal(&MsgProposal{
+		View:                   x.View,
+		From:                   x.From,
+		ProposedBlock:          x.ProposedBlock,
+		BlockHash:              x.BlockHash,
+		RoundChangeCertificate: x.RoundChangeCertificate,
+	})
+
+	return bz
+}
+
+func (x *MsgPrepare) Bytes() []byte {
+	bz, _ := proto.Marshal(&MsgPrepare{
+		View:      x.View,
+		From:      x.From,
+		Signature: x.Signature,
+		BlockHash: x.BlockHash,
+	})
+
+	return bz
+}
+
+func (x *MsgPrepare) Payload() []byte {
+	bz, _ := proto.Marshal(&MsgPrepare{
+		View:      x.View,
+		From:      x.From,
+		BlockHash: x.BlockHash,
+	})
+
+	return bz
+}
+
+func (x *MsgCommit) Bytes() []byte {
+	bz, _ := proto.Marshal(&MsgCommit{
+		View:       x.View,
+		From:       x.From,
+		Signature:  x.Signature,
+		BlockHash:  x.BlockHash,
+		CommitSeal: x.CommitSeal,
+	})
+
+	return bz
+}
+
+func (x *MsgCommit) Payload() []byte {
+	bz, _ := proto.Marshal(&MsgCommit{
+		View:       x.View,
+		From:       x.From,
+		BlockHash:  x.BlockHash,
+		CommitSeal: x.CommitSeal,
+	})
+
+	return bz
+}
+
+func (x *MsgRoundChange) Bytes() []byte {
+	bz, _ := proto.Marshal(&MsgRoundChange{
+		View:                        x.View,
+		From:                        x.From,
+		Signature:                   x.Signature,
+		LatestPreparedProposedBlock: x.LatestPreparedProposedBlock,
+		LatestPreparedCertificate:   x.LatestPreparedCertificate,
+	})
+
+	return bz
+}
+
+func (x *MsgRoundChange) Payload() []byte {
+	bz, _ := proto.Marshal(&MsgRoundChange{
+		View:                        x.View,
+		From:                        x.From,
+		LatestPreparedProposedBlock: x.LatestPreparedProposedBlock,
+		LatestPreparedCertificate:   x.LatestPreparedCertificate,
+	})
+
+	return bz
+}
+
+func (x *ProposedBlock) Bytes() []byte {
+	bz, _ := proto.Marshal(x)
+	return bz
+}
+
+func (rcc *RoundChangeCertificate) HighestRoundBlock() ([]byte, uint64) {
+	roundsAndPreparedBlocks := make(map[uint64][]byte)
+
+	for _, msg := range rcc.Messages {
+		pb := msg.LatestPreparedProposedBlock
+		pc := msg.LatestPreparedCertificate
+
+		if pb == nil || pc == nil {
+			continue
+		}
+
+		roundsAndPreparedBlocks[pc.ProposalMessage.View.Round] = pb.Block
+	}
+
+	if len(roundsAndPreparedBlocks) == 0 {
+		return nil, 0
+	}
+
+	var (
+		maxRound      uint64
+		maxRoundBlock []byte
+	)
+
+	for round, block := range roundsAndPreparedBlocks {
+		if round >= maxRound {
+			maxRound = round
+			maxRoundBlock = block
+		}
+	}
+
+	return maxRoundBlock, maxRound
+}
+
+func (rcc *RoundChangeCertificate) HighestRoundBlockHash() ([]byte, uint64) {
+	roundsAndPreparedBlockHashes := make(map[uint64][]byte)
+	for _, msg := range rcc.Messages {
+		pc := msg.LatestPreparedCertificate
+		if pc == nil {
+			continue
+		}
+
+		roundsAndPreparedBlockHashes[pc.ProposalMessage.View.Round] = pc.ProposalMessage.BlockHash
+	}
+
+	if len(roundsAndPreparedBlockHashes) == 0 {
+		return nil, 0
+	}
+
+	var (
+		maxRound             uint64
+		maxRoundProposalHash []byte
+	)
+
+	for round, proposalHash := range roundsAndPreparedBlockHashes {
+		if round >= maxRound {
+			maxRound = round
+			maxRoundProposalHash = proposalHash
+		}
+	}
+
+	return maxRoundProposalHash, maxRound
 }
