6ae7303fbac58be8795259fbb18a3ca156bf8708
Dusan Brajovic <dbrajovic3@gmail.com>
2023-09-06 23:18:04 +0200

    tests

--- message/store/store_test.go
+++ message/store/store_test.go
@@ -27,119 +27,191 @@ type testTable[M msg] struct {
 	expectedErr error
 }
 
-func TestAddMessage(t *testing.T) {
+func TestStore_MsgProposal(t *testing.T) {
 	t.Parallel()
 
-	t.Run("MsgProposal", func(t *testing.T) {
-		t.Parallel()
+	testTable := []testTable[types.MsgProposal]{
+		{
+			name: "invalid signature",
+			codec: mockCodec{func(_ []byte, sig []byte) []byte {
+				if bytes.Equal(sig, []byte("signature")) {
+					return []byte("from")
+				}
 
-		testTable := []testTable[types.MsgProposal]{
-			{
-				name: "invalid signature",
-				codec: mockCodec{func(_ []byte, sig []byte) []byte {
-					if bytes.Equal(sig, []byte("signature")) {
-						return []byte("from")
-					}
+				return nil
+			}},
+			msg: &types.MsgProposal{
+				From:      []byte("bad from"),
+				Signature: []byte("bad signature"),
+			},
+			expectedErr: ErrInvalidSignature,
+			runTestFn: func(store *Store, msg *types.MsgProposal) error {
+				return store.AddMsgProposal(msg)
+			},
+		},
 
-					return nil
-				}},
-				msg: &types.MsgProposal{
-					From:      []byte("bad from"),
-					Signature: []byte("bad signature"),
-				},
-				expectedErr: ErrInvalidSignature,
-				runTestFn: func(store *Store, msg *types.MsgProposal) error {
-					return store.AddMsgProposal(msg)
-				},
+		{
+			name: "msg added",
+			codec: mockCodec{func(_ []byte, sig []byte) []byte {
+				if bytes.Equal(sig, []byte("signature")) {
+					return []byte("from")
+				}
+
+				return nil
+			}},
+			msg: &types.MsgProposal{
+				View:      &types.View{Sequence: 101, Round: 0},
+				From:      []byte("from"),
+				Signature: []byte("signature"),
+			},
+			runTestFn: func(store *Store, msg *types.MsgProposal) error {
+				return store.AddMsgProposal(msg)
+			},
+		},
+
+		{
+			name: "no duplicate msg when added twice",
+			codec: mockCodec{func(_ []byte, sig []byte) []byte {
+				if bytes.Equal(sig, []byte("signature")) {
+					return []byte("from")
+				}
+
+				return nil
+			}},
+			msg: &types.MsgProposal{
+				View:      &types.View{Sequence: 101, Round: 0},
+				From:      []byte("from"),
+				Signature: []byte("signature"),
+			},
+			runTestFn: func(store *Store, msg *types.MsgProposal) error {
+				require.NoError(t, store.AddMsgProposal(msg))
+				require.NoError(t, store.AddMsgProposal(msg))
+
+				if len(store.GetProposalMessages(msg.View)) != 1 {
+					t.Fatal("duplicate msg in collection")
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name: "2 messages with different round",
+			codec: mockCodec{func(_ []byte, sig []byte) []byte {
+				if bytes.Equal(sig, []byte("signature")) {
+					return []byte("from")
+				}
+
+				if bytes.Equal(sig, []byte("other signature")) {
+					return []byte("other from")
+				}
+
+				return nil
+			}},
+			msg: &types.MsgProposal{
+				View:      &types.View{Sequence: 101, Round: 0},
+				From:      []byte("from"),
+				Signature: []byte("signature"),
 			},
 
-			{
-				name: "msg added",
-				codec: mockCodec{func(_ []byte, sig []byte) []byte {
-					if bytes.Equal(sig, []byte("signature")) {
-						return []byte("from")
-					}
+			runTestFn: func(store *Store, msg *types.MsgProposal) error {
+				msg2 := &types.MsgProposal{
+					View:      &types.View{Sequence: 101, Round: 1},
+					From:      []byte("other from"),
+					Signature: []byte("other signature"),
+				}
 
-					return nil
-				}},
-				msg: &types.MsgProposal{
+				require.NoError(t, store.AddMsgProposal(msg))
+				require.NoError(t, store.AddMsgProposal(msg2))
+
+				if len(store.GetProposalMessages(msg.View)) != 1 ||
+					len(store.GetProposalMessages(msg2.View)) != 1 {
+					return errors.New("messages not found")
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name: "2 messages with different sequence",
+			codec: mockCodec{func(_ []byte, sig []byte) []byte {
+				if bytes.Equal(sig, []byte("signature")) {
+					return []byte("from")
+				}
+
+				if bytes.Equal(sig, []byte("other signature")) {
+					return []byte("other from")
+				}
+
+				return nil
+			}},
+			msg: &types.MsgProposal{
+				View:      &types.View{Sequence: 101, Round: 0},
+				From:      []byte("from"),
+				Signature: []byte("signature"),
+			},
+
+			runTestFn: func(store *Store, msg *types.MsgProposal) error {
+				msg2 := &types.MsgProposal{
+					View:      &types.View{Sequence: 102, Round: 0},
+					From:      []byte("other from"),
+					Signature: []byte("other signature"),
+				}
+
+				require.NoError(t, store.AddMsgProposal(msg))
+				require.NoError(t, store.AddMsgProposal(msg2))
+
+				if len(store.GetProposalMessages(msg.View)) != 1 ||
+					len(store.GetProposalMessages(msg2.View)) != 1 {
+					return errors.New("messages not found")
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name: "2 unique messages with same view",
+			codec: mockCodec{func(_ []byte, sig []byte) []byte {
+				if bytes.Equal(sig, []byte("signature")) {
+					return []byte("from")
+				}
+
+				if bytes.Equal(sig, []byte("other signature")) {
+					return []byte("other from")
+				}
+
+				return nil
+			}},
+			msg: &types.MsgProposal{
+				View:      &types.View{Sequence: 101, Round: 0},
+				From:      []byte("from"),
+				Signature: []byte("signature"),
+			},
+			runTestFn: func(store *Store, msg *types.MsgProposal) error {
+				require.NoError(t, store.AddMsgProposal(msg))
+				require.NoError(t, store.AddMsgProposal(&types.MsgProposal{
 					View:      &types.View{Sequence: 101, Round: 0},
-					From:      []byte("from"),
-					Signature: []byte("signature"),
-				},
-				runTestFn: func(store *Store, msg *types.MsgProposal) error {
-					return store.AddMsgProposal(msg)
-				},
+					From:      []byte("other from"),
+					Signature: []byte("other signature"),
+				}))
+
+				if len(store.GetProposalMessages(msg.View)) != 2 {
+					return errors.New("only 1 message in store")
+				}
+
+				return nil
 			},
+		},
+	}
 
-			{
-				name: "no duplicate msg when added twice",
-				codec: mockCodec{func(_ []byte, sig []byte) []byte {
-					if bytes.Equal(sig, []byte("signature")) {
-						return []byte("from")
-					}
+	for _, tt := range testTable {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-					return nil
-				}},
-				msg: &types.MsgProposal{
-					View:      &types.View{Sequence: 101, Round: 0},
-					From:      []byte("from"),
-					Signature: []byte("signature"),
-				},
-				runTestFn: func(store *Store, msg *types.MsgProposal) error {
-					require.NoError(t, store.AddMsgProposal(msg))
-					require.NoError(t, store.AddMsgProposal(msg))
-
-					if len(store.GetProposalMessages(msg.View)) != 1 {
-						t.Fatal("duplicate msg in collection")
-					}
-
-					return nil
-				},
-			},
-
-			{
-				name: "2 unique messages with same view",
-				codec: mockCodec{func(_ []byte, sig []byte) []byte {
-					if bytes.Equal(sig, []byte("signature")) {
-						return []byte("from")
-					}
-
-					if bytes.Equal(sig, []byte("other signature")) {
-						return []byte("other from")
-					}
-
-					return nil
-				}},
-				msg: &types.MsgProposal{
-					View:      &types.View{Sequence: 101, Round: 0},
-					From:      []byte("from"),
-					Signature: []byte("signature"),
-				},
-				runTestFn: func(store *Store, msg *types.MsgProposal) error {
-					require.NoError(t, store.AddMsgProposal(msg))
-					require.NoError(t, store.AddMsgProposal(&types.MsgProposal{
-						View:      &types.View{Sequence: 101, Round: 0},
-						From:      []byte("other from"),
-						Signature: []byte("other signature"),
-					}))
-
-					if len(store.GetProposalMessages(msg.View)) != 2 {
-						return errors.New("only 1 message in store")
-					}
-
-					return nil
-				},
-			},
-		}
-
-		for _, tt := range testTable {
-			tt := tt
-			t.Run(tt.name, func(t *testing.T) {
-				t.Parallel()
-
-				assert.ErrorIs(t, tt.runTestFn(New(tt.codec), tt.msg), tt.expectedErr)
-			})
-		}
-	})
+			assert.ErrorIs(t, tt.runTestFn(New(tt.codec), tt.msg), tt.expectedErr)
+		})
+	}
 }
