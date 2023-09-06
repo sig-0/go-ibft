68de8939496f32aba25bc1ac896b42b08e6ff120
Dusan Brajovic <dbrajovic3@gmail.com>
2023-09-06 18:14:46 +0200

    impl unique From constraint + test

--- message/store/store.go
+++ message/store/store.go
@@ -13,7 +13,16 @@ type Codec interface {
 type Store struct {
 	cdc Codec
 
-	proposal map[uint64]map[uint64][]*types.MsgProposal
+	proposal map[uint64]map[uint64]map[string]*types.MsgProposal
+}
+
+func New(cdc Codec) *Store {
+	s := &Store{
+		cdc:      cdc,
+		proposal: map[uint64]map[uint64]map[string]*types.MsgProposal{},
+	}
+
+	return s
 }
 
 func (s *Store) AddMsgProposal(msg *types.MsgProposal) error {
@@ -21,14 +30,33 @@ func (s *Store) AddMsgProposal(msg *types.MsgProposal) error {
 		return errors.New("invalid signature")
 	}
 
+	// init once for every new sequence
+	sequenceMessages, ok := s.proposal[msg.View.Sequence]
+	if !ok {
+		s.proposal[msg.View.Sequence] = map[uint64]map[string]*types.MsgProposal{}
+		sequenceMessages = s.proposal[msg.View.Sequence]
+	}
+
+	// init once for every new round
+	roundMessages, ok := sequenceMessages[msg.View.Round]
+	if !ok {
+		sequenceMessages[msg.View.Round] = map[string]*types.MsgProposal{}
+		roundMessages = sequenceMessages[msg.View.Round]
+	}
+
+	roundMessages[string(msg.From)] = msg
+
 	return nil
 }
 
-func New(cdc Codec) *Store {
-	s := &Store{
-		cdc:      cdc,
-		proposal: map[uint64]map[uint64][]*types.MsgProposal{},
+func (s *Store) GetProposalMessages(view *types.View) []*types.MsgProposal {
+	// todo: nil cases
+
+	messages := make([]*types.MsgProposal, 0)
+	for _, msg := range s.proposal[view.Sequence][view.Round] {
+		messages = append(messages, msg)
+
 	}
 
-	return s
+	return messages
 }
--- message/store/store_test.go
+++ message/store/store_test.go
@@ -1,8 +1,10 @@
 package store
 
 import (
+	"bytes"
 	"github.com/madz-lab/go-ibft/message/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -17,12 +19,11 @@ func (c mockCodec) RecoverFrom(data []byte, sig []byte) []byte {
 func TestAddMessage(t *testing.T) {
 	t.Parallel()
 
-	t.Run("MsgProposal", func(t *testing.T) {
+	t.Run("invalid MsgProposal", func(t *testing.T) {
 		t.Parallel()
 
 		msg := &types.MsgProposal{
-			View:                   nil,
-			From:                   nil,
+			From:                   []byte("from"),
 			Signature:              nil,
 			ProposedBlock:          nil,
 			BlockHash:              nil,
@@ -34,5 +35,90 @@ func TestAddMessage(t *testing.T) {
 		}})
 
 		assert.Error(t, store.AddMsgProposal(msg))
+	})
+
+	t.Run("add MsgProposal", func(t *testing.T) {
+		t.Parallel()
+
+		msg := &types.MsgProposal{
+			View:      &types.View{Sequence: 101, Round: 1},
+			From:      []byte("from"),
+			Signature: nil,
+		}
+
+		store := New(mockCodec{recoverFn: func(_ []byte, _ []byte) []byte {
+			return []byte("from")
+		}})
+
+		assert.NoError(t, store.AddMsgProposal(msg))
+		msgs := store.GetProposalMessages(&types.View{Sequence: 101, Round: 1})
+		require.Equal(t, msg, msgs[0])
+	})
+
+	t.Run("add 2 MsgProposal", func(t *testing.T) {
+		t.Parallel()
+
+		msg1 := &types.MsgProposal{
+			View:      &types.View{Sequence: 101, Round: 1},
+			From:      []byte("from1"),
+			Signature: []byte("sig1"),
+		}
+
+		msg2 := &types.MsgProposal{
+			View:      &types.View{Sequence: 101, Round: 1},
+			From:      []byte("from2"),
+			Signature: []byte("sig2"),
+		}
+
+		store := New(mockCodec{recoverFn: func(_ []byte, sig []byte) []byte {
+			if bytes.Equal(sig, []byte("sig1")) {
+				return []byte("from1")
+			}
+
+			if bytes.Equal(sig, []byte("sig2")) {
+				return []byte("from2")
+			}
+
+			return nil
+		}})
+
+		assert.NoError(t, store.AddMsgProposal(msg1))
+		assert.NoError(t, store.AddMsgProposal(msg2))
+
+		msgs := store.GetProposalMessages(&types.View{Sequence: 101, Round: 1})
+		require.Len(t, msgs, 2)
+		assert.Equal(t, msg1, msgs[0])
+		assert.Equal(t, msg2, msgs[1])
+	})
+
+	t.Run("add 2 identical MsgProposal", func(t *testing.T) {
+		t.Parallel()
+
+		msg1 := &types.MsgProposal{
+			View:      &types.View{Sequence: 101, Round: 1},
+			From:      []byte("from1"),
+			Signature: []byte("sig1"),
+		}
+
+		msg2 := &types.MsgProposal{
+			View:      &types.View{Sequence: 101, Round: 1},
+			From:      []byte("from1"),
+			Signature: []byte("sig1"),
+		}
+
+		store := New(mockCodec{recoverFn: func(_ []byte, sig []byte) []byte {
+			if bytes.Equal(sig, []byte("sig1")) {
+				return []byte("from1")
+			}
+
+			return nil
+		}})
+
+		assert.NoError(t, store.AddMsgProposal(msg1))
+		assert.NoError(t, store.AddMsgProposal(msg2))
+
+		msgs := store.GetProposalMessages(&types.View{Sequence: 101, Round: 1})
+		require.Len(t, msgs, 1)
+		assert.Equal(t, msg1, msgs[0])
 	})
 }
